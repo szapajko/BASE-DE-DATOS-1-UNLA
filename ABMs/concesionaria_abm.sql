2af3ad46263bf2ec1f42f5355b9ead3116339c2a
szapajko <164340841+szapajko@users.noreply.github.com>
2026-09-14 15:13:30 -0300

    agrego abm y test de concesionaria

---
+++ ABMs/concesionaria_abm.sql
@@ -0,0 +1,75 @@
+USE `tp_bd1`;
+
+DELIMITER //
+
+
+DROP PROCEDURE IF EXISTS altaConcesionaria //
+
+CREATE PROCEDURE altaConcesionaria(
+    OUT nResultado INT,
+    OUT cMensaje VARCHAR(100),
+    IN c_razon_social VARCHAR(45),
+    IN c_nombre VARCHAR(45)
+)
+BEGIN 
+    IF EXISTS (SELECT 1 FROM concesionaria WHERE razon_social = c_razon_social) THEN
+        SET nResultado = -1;
+        SET cMensaje = 'RAZON SOCIAL YA REGISTRADA';
+    ELSE
+        INSERT INTO concesionaria (razon_social, nombre)
+        VALUES (c_razon_social, c_nombre);
+        
+        SET nResultado = 0;
+        SET cMensaje = 'ELEMENTO CARGADO CON EXITO';
+    END IF;
+END //
+
+
+DROP PROCEDURE IF EXISTS modificarConcesionaria //
+
+CREATE PROCEDURE modificarConcesionaria(
+    OUT nResultado INT,
+    OUT cMensaje VARCHAR(100),
+    IN c_id_concesionaria INT,
+    IN c_razon_social VARCHAR(45),
+    IN c_nombre VARCHAR(45)
+)
+BEGIN
+	IF NOT EXISTS(SELECT 1 FROM concesionaria c where c.id_concesionaria = c_id_concesionaria)THEN
+		SET nResultado = -2;
+        SET cMensaje = "ERROR, CONCESIONARIA INEXISTENTE";
+	ELSEIF EXISTS(SELECT 1 FROM concesionaria c where c.razon_social = c_razon_social AND c_id_concesionaria <> c.id_concesionaria) THEN
+			SET nResultado = -3;
+			SET cMensaje = "ERROR, CONCESIONARIA EXISTENTE CON MISMA RAZON SOCIAL";
+	ELSE
+		UPDATE concesionaria
+        SET razon_social = c_razon_social,
+			nombre= c_nombre
+		WHERE id_concesionaria = c_id_concesionaria;
+		SET nResultado = 0;
+        SET cMensaje = 'ELEMENTO MODIFICADO CON EXITO';
+    END IF;
+END //
+
+
+DROP PROCEDURE IF EXISTS bajaConcesionaria //
+
+CREATE PROCEDURE bajaConcesionaria(
+    OUT nResultado INT,
+    OUT cMensaje VARCHAR(100),
+    IN c_id_concesionaria INT
+)
+BEGIN
+	IF EXISTS (SELECT 1 FROM pedido p WHERE p.id_concesionaria = c_id_concesionaria) THEN
+		SET nResultado = -4;
+        SET cMensaje = "ERROR, CONCESIONARIA CON PEDIDOS REGISTRADOS";
+	ELSEIF NOT EXISTS(SELECT 1 FROM concesionaria where id_concesionaria=c_id_concesionaria) THEN
+			SET nResultado = -2;
+			SET cMensaje = "ERROR, CONCESIONARIA INEXISTENTE";		
+	ELSE
+		DELETE FROM concesionaria WHERE id_concesionaria = c_id_concesionaria;
+		SET nResultado = 0;
+        SET cMensaje = 'ELEMENTO ELIMINADO CON EXITO';
+    END IF;
+END //
+DELIMITER ;
---
+++ test/concesionaria_abm_test.sql
@@ -0,0 +1,51 @@
+USE `tp_bd1`;
+
+-- PRUEBAS: ABM CONCESIONARIA
+
+-- CASO 1: Alta Exitosa
+CALL altaConcesionaria(@res, @msg, 'Auto del Valle S.A.', 'Central Quilmes');
+SELECT 'CASO 1: Alta Exitosa' AS Test, @res AS Codigo, @msg AS Mensaje;
+
+SELECT * FROM concesionaria WHERE razon_social = 'Auto del Valle S.A.';
+
+
+-- CASO 2: Alta Rechazada por Razón Social Duplicada
+CALL altaConcesionaria(@res, @msg, 'Auto del Valle S.A.', 'Sucursal Berazategui');
+SELECT 'CASO 2: Alta Duplicada' AS Test, @res AS Codigo, @msg AS Mensaje;
+
+
+-- CASO 3: Modificación Exitosa
+SET @id_test = (SELECT id_concesionaria FROM concesionaria WHERE razon_social = 'Auto del Valle S.A.' LIMIT 1);
+
+CALL modificarConcesionaria(@res, @msg, @id_test, 'Auto del Valle Motors S.A.', 'Casa Central');
+SELECT 'CASO 3: Modificación Exitosa' AS Test, @res AS Codigo, @msg AS Mensaje;
+SELECT * FROM concesionaria WHERE id_concesionaria = @id_test;
+
+
+
+-- CASO 4: Modificación Rechazada (ID Inexistente)
+CALL modificarConcesionaria(@res, @msg, 99999, 'Fantasma S.A.', 'Sucursal Inexistente');
+SELECT 'CASO 4: Modificación ID Inexistente' AS Test, @res AS Codigo, @msg AS Mensaje;
+
+
+-- CASO 5: Baja Rechazada por Integridad (Tiene Pedidos)
+-- Creamos un pedido asociado
+INSERT INTO pedido (id_concesionaria, num_pedido, fecha, fecha_entrega_estimada)
+VALUES (@id_test, 9001, '2026-09-10', '2026-09-30');
+
+CALL bajaConcesionaria(@res, @msg, @id_test);
+SELECT 'CASO 5: Baja con Pedido Asociado' AS Test, @res AS Codigo, @msg AS Mensaje;
+
+
+-- CASO 6: Baja Exitosa
+-- Eliminamos el pedido de prueba
+DELETE FROM pedido WHERE num_pedido = 9001;
+CALL bajaConcesionaria(@res, @msg, @id_test);
+SELECT 'CASO 6: Baja Exitosa' AS Test, @res AS Codigo, @msg AS Mensaje;
+
+-- CASO 7: Baja rechadaza por Concesionaria Inexistente
+CALL bajaConcesionaria(@res, @msg, @id_test);
+SELECT 'CASO 7: Baja Inexistente' AS Test, @res AS Codigo, @msg AS Mensaje;
+
+-- La tabla debe estar vacía nuevamente
+SELECT * FROM concesionaria;
