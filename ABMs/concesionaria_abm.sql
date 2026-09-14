a0177ebdef63fdb67489221d1458a6fc8a13a87e
szapajko <164340841+szapajko@users.noreply.github.com>
2026-09-14 16:32:28 -0300

    modificacion sobre el manejo de errores

--- ABMs/concesionaria_abm.sql
+++ ABMs/concesionaria_abm.sql
@@ -8,19 +8,23 @@ DROP PROCEDURE IF EXISTS altaConcesionaria //
 CREATE PROCEDURE altaConcesionaria(
     OUT nResultado INT,
     OUT cMensaje VARCHAR(100),
+    IN c_id_concesionaria INT,
     IN c_razon_social VARCHAR(45),
     IN c_nombre VARCHAR(45)
 )
 BEGIN 
-    IF EXISTS (SELECT 1 FROM concesionaria WHERE razon_social = c_razon_social) THEN
+    IF EXISTS (SELECT 1 FROM concesionaria WHERE id_concesionaria = c_id_concesionaria) THEN
         SET nResultado = -1;
-        SET cMensaje = 'RAZON SOCIAL YA REGISTRADA';
+        SET cMensaje = 'ERROR: CONCESIONARIA YA REGISTRADA CON MISMO ID';
+    ELSEIF EXISTS (SELECT 1 FROM concesionaria WHERE razon_social = c_razon_social) THEN
+        SET nResultado = -1;
+        SET cMensaje = 'ERROR: YA EXISTE UNA CONCESIONARIA CON LA MISMA RAZON SOCIAL';
     ELSE
-        INSERT INTO concesionaria (razon_social, nombre)
-        VALUES (c_razon_social, c_nombre);
+        INSERT INTO concesionaria (id_concesionaria, razon_social, nombre)
+        VALUES (c_id_concesionaria, c_razon_social, c_nombre);
         
         SET nResultado = 0;
-        SET cMensaje = 'ELEMENTO CARGADO CON EXITO';
+        SET cMensaje = ''; 
     END IF;
 END //
 
@@ -36,10 +40,10 @@ CREATE PROCEDURE modificarConcesionaria(
 )
 BEGIN
 	IF NOT EXISTS(SELECT 1 FROM concesionaria c where c.id_concesionaria = c_id_concesionaria)THEN
-		SET nResultado = -2;
+		SET nResultado = -1;
         SET cMensaje = "ERROR, CONCESIONARIA INEXISTENTE";
 	ELSEIF EXISTS(SELECT 1 FROM concesionaria c where c.razon_social = c_razon_social AND c_id_concesionaria <> c.id_concesionaria) THEN
-			SET nResultado = -3;
+			SET nResultado = -1;
 			SET cMensaje = "ERROR, CONCESIONARIA EXISTENTE CON MISMA RAZON SOCIAL";
 	ELSE
 		UPDATE concesionaria
@@ -47,7 +51,7 @@ BEGIN
 			nombre= c_nombre
 		WHERE id_concesionaria = c_id_concesionaria;
 		SET nResultado = 0;
-        SET cMensaje = 'ELEMENTO MODIFICADO CON EXITO';
+        SET cMensaje = '';
     END IF;
 END //
 
@@ -61,15 +65,15 @@ CREATE PROCEDURE bajaConcesionaria(
 )
 BEGIN
 	IF EXISTS (SELECT 1 FROM pedido p WHERE p.id_concesionaria = c_id_concesionaria) THEN
-		SET nResultado = -4;
+		SET nResultado = -1;
         SET cMensaje = "ERROR, CONCESIONARIA CON PEDIDOS REGISTRADOS";
 	ELSEIF NOT EXISTS(SELECT 1 FROM concesionaria where id_concesionaria=c_id_concesionaria) THEN
-			SET nResultado = -2;
+			SET nResultado = -1;
 			SET cMensaje = "ERROR, CONCESIONARIA INEXISTENTE";		
 	ELSE
 		DELETE FROM concesionaria WHERE id_concesionaria = c_id_concesionaria;
 		SET nResultado = 0;
-        SET cMensaje = 'ELEMENTO ELIMINADO CON EXITO';
+        SET cMensaje = '';
     END IF;
 END //
 DELIMITER ;
